ba73d7c8063bc3bcc1152d7f3d8437c6f1170680
Michael Hale <mike@hales.ws>
2014-09-19 21:56:29 -0400

    bump version

--- lib/excon/middleware/aws/exponential_backoff/version.rb
+++ lib/excon/middleware/aws/exponential_backoff/version.rb
@@ -4,7 +4,7 @@ module Excon
   module Middleware
     module AWS
       class ExponentialBackoff < Excon::Middleware::Base
-        VERSION = "0.0.1"
+        VERSION = "0.0.2"
       end
     end
   end
