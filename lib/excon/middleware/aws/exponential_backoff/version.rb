002c5b70dc830337bc3f72759bc31c1ce026dcb2
Michael Hale <mike@hales.ws>
2015-06-26 18:05:03 -0400

    remove excon require dep

--- lib/excon/middleware/aws/exponential_backoff/version.rb
+++ lib/excon/middleware/aws/exponential_backoff/version.rb
@@ -1,6 +1,9 @@
-require "excon"
-
 module Excon
+  # Hack to allow us to reference Excon::Middleware::Base without having to require excon when this file is used by gemspec
+  module Middleware
+    class Base;end
+  end
+
   module Middleware
     module AWS
       class ExponentialBackoff < Excon::Middleware::Base
